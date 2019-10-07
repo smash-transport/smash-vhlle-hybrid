86b6adcbd2fb69001353140aa5f0f90036af83be
Anna Schäfer <aschaefer@fias.uni-frankfurt.de>
2019-10-07 14:51:28 +0200

    Add python script to reformat vHLLE output to be fed into Sangwook's sampler

---
+++ python_scripts/extract_hypersurface_contributions.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+'''
+    This script reads in the freezeout hypersurface created by CORNELIUS within
+    vHLLE and reformats it such that:
+    - No trailing whitespaces at the beginning of the line
+    - Different line entries are separated by only whitespace
+    - Hypersurface elements with vanishing energy density or temperature are
+      removed
+
+    The reformatted file can be applied within Sangwook's Cooper-Frye sampler
+    for particlization.
+'''
+
+hypersurface_contributions = open('freezeout_contributions_only.dat', 'w')
+
+with open("freezeout.dat", 'r') as f:
+    for line in f:
+        line = re.sub(' +', ' ',line) # remove whitespaces between entries
+        newline = line[1:]
+        line = line.split()
+
+        if ((float(line[13]) > 0.0) and (float(line[12]) > 0.0)):
+            # if non-zero energy-density and temperature, print to hypersurface
+            # file that contains only non-zero contributions
+            hypersurface_contributions.write(newline)
+
+hypersurface_contributions.close()
