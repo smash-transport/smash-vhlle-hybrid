e63ddc4b4898331aade58417acc8a23299561281
ren <hirayama@fias.uni-frankfurt.de>
2023-05-30 15:21:57 +0200

    Create check for system requirements and add to unit test

--- bash/system_requirements.bash
+++ bash/system_requirements.bash
@@ -7,7 +7,93 @@
 #
 #===================================================
 
+
+function __static__Declare_System_Requirements()
+{
+    declare -grA HYBRID_systemRequirements=(
+        ['bash']='4.4.0'
+        ['awk']='4.1.0'
+        ['sed']='4.2.1'
+	['tput']='5.9' # the last number is a date
+    )
+}
+
 function Check_System_Requirements()
 {
-    Print_Not_Implemented_Function_Error
+    local program requirements_present
+    requirements_present=0
+    declare -A system_found_versions
+    __static__Declare_System_Requirements
+    for program in "${!HYBRID_systemRequirements[@]}"; do
+        if ! __static__Try_Find_Requirement "${program}"; then
+	    echo "${program}" 'not found! Minimum version' "${HYBRID_systemRequirements[${program}]}" 'is required.'
+	    requirements_present=1
+	    continue
+	fi
+	if ! __static__Try_Find_Version "${program}"; then
+	    echo 'Unable to find version of ' "${program}" ', skipping version check! Please ensure that '\
+	         'current version is at least ' "${HYBRID_systemRequirements[${program}]}"  
+	    continue
+	fi
+	if ! __static__Check_Version_Suffices "${program}"; then
+	    echo "${program}" 'version' "${system_found_versions[${program}]}" 'found, but version'\
+	         "${HYBRID_systemRequirements[${program}]}" 'is required.'
+	    requirements_present=1
+        fi
+    done
+    if [[ ${requirements_present} -ne 0 ]]; then
+        Print_Fatal_And_Exit 'Please install (maybe locally) the required versions of the above programs.'
+    else
+	return ${requirements_present}
+    fi
+
+}
+
+function __static__Try_Find_Requirement()
+{
+    if hash $1 2>/dev/null; then
+        return 0
+    else
+	return 1
+    fi
+}
+
+function __static__Try_Find_Version()
+{
+    local found_version
+    case "$1" in
+        bash )
+            found_version="$(sed 's/ /./g' <<< "${BASH_VERSINFO[@]:0:3}")"
+	    ;;
+	awk )
+	    found_version=$(awk --version | head -n1 | grep -o "[0-9.]\+" | head -n1)
+	    ;;
+	sed )
+	    found_version=$(sed --version | head -n1 | grep -o "[0-9.]\+" | head -n1)
+	    ;;
+	tput )
+	    found_version=$(tput -V | grep -o "[0-9.]\+" | cut -d'.' -f1,2)
+	    ;;
+    *)
+	return 1
+    esac    
+    if [[ ${found_version} =~ ^[0-9]([.0-9])*$ ]]; then
+        system_found_versions["$1"]="${found_version}"
+    else
+        return 1
+    fi
+}
+
+function __static__Check_Version_Suffices()
+{
+    # Here we assume that the programs follow the semantic versioning standard
+    local required found versioning
+    required=($(echo "${HYBRID_systemRequirements[$1]}" | tr '.' ' '))
+    found=($(echo "${system_found_versions[$1]}" | tr '.' ' '))
+    for versioning in ${!required[@]}; do
+        if [[ "${required[${versioning}]}" -lt "${found[${versioning}]}" ]]; then
+	    return 0
+	fi
+    done
+    return 1
 }
--- tests/unit_tests_system_requirements.bash
+++ tests/unit_tests_system_requirements.bash
@@ -7,7 +7,50 @@
 #
 #===================================================
 
+function __static__Fake_Command_Version()
+{
+    if [[ "$3" = "$1" ]]; then
+        echo $2
+    else
+        Print_Fatal_And_Exit "wrong usage of ${FUNCNAME[1]}"
+    fi
+}
+
+function __static__Inhibit_Commands_Version()
+{
+    function awk()
+    {
+        __static__Fake_Command_Version '--version' "GNU Awk ${version}, API: 3.0 (GNU MPFR 4.1.0, GNU MP 6.2.1)" "$@"
+    }
+
+    function sed()
+    {
+        __static__Fake_Command_Version '--version' "sed (GNU sed) ${version} Packaged by Debian" "$@"
+    }
+
+    function tput()
+    {
+        __static__Fake_Command_Version '-V' "ncurses ${version}" "$@"
+    }
+}
+
 function Unit_Test__system-requirements()
 {
-    return 0
+    __static__Inhibit_Commands_Version
+    local good_version='99.0.0'
+    local bad_version='1.0'
+
+    local version=${good_version}
+    ( Check_System_Requirements &> /dev/null )
+    if [[ $? -ne 0 ]]; then
+        return 1
+    fi
+
+
+    version=${bad_version}
+    ( Check_System_Requirements &> /dev/null )
+    echo $? 
+    if [[ $? -ne 1 ]]; then
+        return 1
+    fi
 }
