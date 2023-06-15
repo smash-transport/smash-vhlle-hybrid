659f2d89c6cea76f383d22f632360dcfeb526b64
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2023-06-15 10:19:21 +0200

    Implement parsing of execution mode

--- bash/command_line_parsers/main_parser.bash
+++ bash/command_line_parsers/main_parser.bash
@@ -9,10 +9,39 @@
 
 function Parse_Execution_Mode()
 {
-    Print_Not_Implemented_Function_Error
+    if [[ "${#HYBRID_command_line_options_to_parse[@]}" -eq 0 ]]; then
+        return 0
+    fi
+    #Locally set function arguments to take advantage of shift
+    set -- "${HYBRID_command_line_options_to_parse[@]}"
+    case "$1" in
+        help | --help )
+            HYBRID_execution_mode='help'
+            ;;
+        version | --version )
+            HYBRID_execution_mode='version'
+            ;;
+        do )
+            HYBRID_execution_mode='do'
+            ;;
+        * )
+            exit_code=${HYBRID_fatal_command_line} Print_Fatal_And_Exit\
+                  "Specified mode '$1' not valid! Run 'Hybrid-handler help' to get further information."
+    esac
+    shift
+    # Update the global array with remaining options to be parsed
+    HYBRID_command_line_options_to_parse=( "$@" )
+    # Ignore any command line option in some specific cases
+    if [[ ${HYBRID_execution_mode} =~ ^(help|version)$ ]]; then
+        HYBRID_command_line_options_to_parse=()
+    elif  Element_In_Array_Equals_To '--help' "${HYBRID_command_line_options_to_parse[@]}"; then
+        HYBRID_execution_mode+='-help'
+        HYBRID_command_line_options_to_parse=()
+    fi
 }
 
 function Parse_Command_Line_Options()
 {
     Print_Not_Implemented_Function_Error
+    return 1
 }
--- tests/unit_tests_command_line_parser.bash
+++ tests/unit_tests_command_line_parser.bash
@@ -7,12 +7,64 @@
 #
 #===================================================
 
-function Unit_Test__parse-command-line-options()
+function Make_Test_Preliminary_Operations__parse-execution-mode()
 {
-    return 0
+    local file_to_be_sourced list_of_files
+    list_of_files=(
+        'command_line_parsers/main_parser.bash'
+        'global_variables.bash'
+    )
+    for file_to_be_sourced in "${list_of_files[@]}"; do
+        source "${HYBRIDT_repository_top_level_path}/bash/${file_to_be_sourced}" || exit ${HYBRID_fatal_builtin}
+    done
+    Define_Further_Global_Variables
 }
 
+function __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success()
+(
+    local expected_option=$1\
+          expected_size=$2\
+          first_option="${HYBRID_command_line_options_to_parse[0]}"
+    Parse_Execution_Mode
+    if [[ $? -ne 0 ]] ||\
+       [[ "${HYBRID_execution_mode}" != "${expected_option}" ]] ||\
+       [[ ${#HYBRID_command_line_options_to_parse[@]} -ne "${expected_size}" ]]; then
+        Print_Error "Parsing of valid execution mode '${first_option}' failed."
+        return 1
+    fi
+)
+
 function Unit_Test__parse-execution-mode()
+{
+    HYBRID_command_line_options_to_parse=()
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'help' 0 || return 1
+    HYBRID_command_line_options_to_parse=( 'help' )
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'help' 0 || return 1
+    HYBRID_command_line_options_to_parse=( '--help' )
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'help' 0 || return 1
+    HYBRID_command_line_options_to_parse=( 'version' )
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'version' 0 || return 1
+    HYBRID_command_line_options_to_parse=( '--version' )
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'version' 0 || return 1
+    HYBRID_command_line_options_to_parse=( 'help' 'with-invalid' 'irrelevant-options' )
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'help' 0 || return 1
+    HYBRID_command_line_options_to_parse=( 'version' 'with-invalid' 'irrelevant-options' )
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'version' 0 || return 1
+    HYBRID_command_line_options_to_parse=( 'do' )
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'do' 0 || return 1
+    HYBRID_command_line_options_to_parse=( 'do' '-o' '/path/to/folder' )
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'do' 2 || return 1
+    HYBRID_command_line_options_to_parse=( 'do' '-o' '/path/to/folder' '--help' )
+    __static__Test_Parsing_Of_Execution_Mode_In_Subshell_Expecting_Success 'do-help' 0 || return 1
+    HYBRID_command_line_options_to_parse=( 'invalid-mode' )
+    ( Parse_Execution_Mode )
+    if [[ $? -eq 0 ]]; then
+        Print_Error 'Parsing of invalid execution mode succeeded.'
+        return 1
+    fi
+}
+
+function Unit_Test__parse-command-line-options()
 {
     return 0
 }
