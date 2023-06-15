f2504804ae4223fb00a5b35a358b9de57bb588a3
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2023-06-15 15:05:43 +0200

    Implement printing of helper messages to user
    At the moment, only in-terminal messages are used to help the user. A
    general help message is given in 'help' mode and another different one
    is created for each execution mode to explain available options.

---
+++ tests/unit_tests_help_for_user.bash
@@ -0,0 +1,32 @@
+#===================================================
+#
+#    Copyright (c) 2023
+#      SMASH Hybrid Team
+#
+#    GNU General Public License (GPLv3 or later)
+#
+#===================================================
+
+function Make_Test_Preliminary_Operations__give-requested-help()
+{
+    source "${HYBRIDT_repository_top_level_path}/bash/command_line_parsers/helper.bash" || exit ${HYBRID_fatal_builtin}
+    HYBRID_configuration_file='./config.yaml'
+    HYBRID_output_directory='.'
+}
+
+function __static__Run_Helper_Expecting_Success()
+{
+    ( Give_Required_Help )
+    if [[ $? -ne 0 ]]; then
+        Print_Error "Providing help in '${HYBRID_execution_mode}' execution mode failed."
+        return 1
+    fi
+}
+
+function Unit_Test__give-requested-help()
+{
+    HYBRID_execution_mode='help'
+    __static__Run_Helper_Expecting_Success || return 1
+    HYBRID_execution_mode='do-help'
+    __static__Run_Helper_Expecting_Success || return 1
+}
