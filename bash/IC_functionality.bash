d93554bafd265b885ad7a42fc49165c9d9bb5ed6
Jan Hammelmann <jan.hammelmann@live.de>
2023-06-21 17:01:08 +0200

    Add Ensure_All_Needed_Input_Exists_IC and Run_Software_IC functionality.

--- bash/IC_functionality.bash
+++ bash/IC_functionality.bash
@@ -9,19 +9,20 @@
 
 function Prepare_Software_Input_File_IC()
 {
-    # check if output directory exists and create it if not
+    # check if output directory exists
     local IC_output_directory="${HYBRID_output_directory}/IC"
     if [[ ! -d "${IC_output_directory}" ]]; then
         mkdir "${IC_output_directory}" || exit ${HYBRID_fatal_builtin}
     fi
 
-    echo "${HYBRID_default_configurations_folder}"
-    # check if config already exists in the output directory, exit if yes
-    IC_config_name=$(basename "${HYBRID_software_base_config_file[IC]}")
-    IC_input_file_path="${IC_output_directory}/${IC_config_name}"
+    # check if config already exists
+    local IC_config_name=$(basename "${HYBRID_software_base_config_file[IC]}")
+    local IC_input_file_path="${IC_output_directory}/${IC_config_name}"
     if [[ ! -f "${IC_input_file_path}" ]]; then
         cp "${HYBRID_software_base_config_file[IC]}" "${IC_output_directory}" || exit ${HYBRID_fatal_builtin}
-        # rewrite stuff
+        if [[ "${HYBRID_software_new_input_keys[IC]}" != "" ]]; then
+            Remove_Comments_And_Replace_Provided_Keys_In_Provided_Input_File "YAML" "${IC_input_file_path}" "${HYBRID_software_new_input_keys[IC]}"
+        fi
     else
         exit_code=${HYBRID_fatal_builtin} Print_Fatal_And_Exit\
             "File \"${IC_input_file_path}\" is already there."
@@ -30,10 +31,31 @@ function Prepare_Software_Input_File_IC()
 
 function Ensure_All_Needed_Input_Exists_IC()
 {
-    Print_Not_Implemented_Function_Error
+    # check if path exists
+    local IC_output_directory="${HYBRID_output_directory}/IC"
+    if [[ ! -d "${IC_output_directory}" ]]; then
+        exit_code=${HYBRID_fatal_builtin} Print_Fatal_And_Exit\
+            "Folder \"${IC_output_directory}\" does not exist."
+    fi
+    # check if config exists
+    local IC_config_name=$(basename "${HYBRID_software_base_config_file[IC]}")
+    local IC_input_file_path="${IC_output_directory}/${IC_config_name}"
+    if [[ ! -f "${IC_input_file_path}" ]]; then
+        exit_code=${HYBRID_fatal_builtin} Print_Fatal_And_Exit\
+            "The config \"${IC_input_file_path}\" does not exist."
+    fi
 }
 
 function Run_Software_IC()
 {
-    Print_Not_Implemented_Function_Error
+    local IC_output_directory="${HYBRID_output_directory}/IC"
+    local IC_terminal_output="${HYBRID_output_directory}/IC/Terminal_Output.txt"
+    local IC_config_name=$(basename "${HYBRID_software_base_config_file[IC]}")
+    local IC_input_file_path="${IC_output_directory}/${IC_config_name}"
+    
+    ./"${HYBRID_software_executable[IC]}" \
+       "-i" "${IC_input_file_path}" \
+       "-o" "${IC_output_directory}" \
+       "-n" \
+       >> "${IC_terminal_output}"
 }
--- tests/unit_tests_IC_functionality.bash
+++ tests/unit_tests_IC_functionality.bash
@@ -13,11 +13,11 @@ function Make_Test_Preliminary_Operations__IC-create-input-file()
     list_of_files=(
         'IC_functionality.bash'
         'global_variables.bash'
+        'software_input_functionality.bash'
     )
     for file_to_be_sourced in "${list_of_files[@]}"; do
         source "${HYBRIDT_repository_top_level_path}/bash/${file_to_be_sourced}" || exit ${HYBRID_fatal_builtin}
     done
-    HYBRID_repository_global_path="${HYBRIDT_repository_top_level_path}"
     Define_Further_Global_Variables
 }
 
@@ -28,11 +28,84 @@ function Unit_Test__IC-create-input-file()
         rm -r "${HYBRID_output_directory}"
     fi
     mkdir "${HYBRID_output_directory}"
+
     Prepare_Software_Input_File_IC
     IC_config_name=$(basename "${HYBRID_software_base_config_file[IC]}")
     IC_input_file_path="${HYBRID_output_directory}/IC/${IC_config_name}"
     if [[ ! -f "${IC_input_file_path}" ]]; then
-        Print_Error 'File was not created.'
+        Print_Error 'The config was not properly created.'
         return 1
     fi
+    ( Prepare_Software_Input_File_IC &> /dev/null )
+    if [[ $? -eq 0 ]]; then
+        Print_Error 'Failed checking the existens of the config.'
+        return 1
+    fi
+    return 0
+}
+
+function Make_Test_Preliminary_Operations__IC-check-all-input()
+{
+    Make_Test_Preliminary_Operations__IC-create-input-file
+}
+
+function Unit_Test__IC-check-all-input()
+{
+    HYBRID_output_directory="./test_dir_IC"
+    if [[ -d "${HYBRID_output_directory}" ]]; then
+        rm -r "${HYBRID_output_directory}"
+    fi
+    mkdir "${HYBRID_output_directory}"
+
+    ( Ensure_All_Needed_Input_Exists_IC &> /dev/null )
+    if [[ $? -eq 0 ]]; then
+        Print_Error 'Validation of input directory failed.'
+        return 1
+    fi
+    mkdir "${HYBRID_output_directory}/IC"
+    ( Ensure_All_Needed_Input_Exists_IC &> /dev/null )
+    if [[ $? -eq 0 ]]; then
+        Print_Error 'Validation of the config file failed.'
+        return 1
+    fi
+    return 0
+}
+
+function Make_Test_Preliminary_Operations__IC-test-run-software()
+{
+    Make_Test_Preliminary_Operations__IC-create-input-file
+}
+
+function Unit_Test__IC-test-run-software()
+{
+    HYBRID_output_directory="test_dir_IC"
+    if [[ -d "${HYBRID_output_directory}" ]]; then
+        rm -r "${HYBRID_output_directory}"
+    fi
+    local IC_exec_directory="${HYBRID_output_directory}/IC"
+    mkdir -p "${IC_exec_directory}"
+
+    HYBRID_software_executable[IC]="${HYBRID_output_directory}/dummy_exec_IC.sh"
+    touch "${HYBRID_software_executable[IC]}"
+    echo "#!/bin/bash" >> "${HYBRID_software_executable[IC]}"
+    echo "echo \$@" >> "${HYBRID_software_executable[IC]}"
+    chmod u+x "${HYBRID_software_executable[IC]}"
+
+    local IC_config_name=$(basename "${HYBRID_software_base_config_file[IC]}")
+    local IC_input_file_path="${HYBRID_output_directory}/IC/${IC_config_name}"
+    touch "${IC_input_file_path}"
+
+    Run_Software_IC
+    local IC_terminal_output="${HYBRID_output_directory}/IC/Terminal_Output.txt"
+    if [[ ! -f "${IC_terminal_output}" ]]; then
+        Print_Error 'The terminal output was not created.'
+        return 1
+    fi
+    local terminal_output_result=$(head "${IC_terminal_output}")
+    local correct_result="-i ${IC_input_file_path} -o ${IC_exec_directory} -n"
+    if [[ "${terminal_output_result}" != "${correct_result}" ]]; then
+        Print_Error 'The terminal output has not the expected content.'
+        return 1
+    fi
+    return 0
 }
