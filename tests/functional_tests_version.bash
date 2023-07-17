1d5855aee8aacd667d6c5563700cd208d2002628
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2023-07-17 16:03:03 +0200

    Avoid running version functional test if git is not found

--- tests/functional_tests_version.bash
+++ tests/functional_tests_version.bash
@@ -11,11 +11,19 @@ function Functional_Test__version()
 {
     local version_output git_describe
     version_output=$(Run_Hybrid_Handler_With_Given_Options_In_Subshell 'version')
-    git_describe=$(git -C "${HYBRIDT_repository_top_level_path}" describe --abbrev=0)
-    printf "${version_output}\n"
-    if [[ $(grep -c "${git_describe}" <<< "${version_output}") -gt 0 ]]; then
-        return 0
+    if [[ $? -ne 0 ]]; then
+        Print_Fatal_And_Exit 'Execution of version mode unexpectedly failed.'
+    fi
+    if ! hash git &> /dev/null; then
+        Print_Warning 'Command ' --emph 'git' ' not available, part of test cannot be run.'
     else
-        Print_Error 'Version string is not containing ' --emph "${git_describe}" ' as expected.'
+        git_describe=$(cd "${HYBRIDT_repository_top_level_path}"; git describe --abbrev=0)
+        printf "${version_output}\n"
+        if [[ $(grep -c "${git_describe}" <<< "${version_output}") -gt 0 ]]; then
+            return 0
+        else
+            Print_Error 'Version string is not containing ' --emph "${git_describe}" ' as expected.'
+            return 1
+        fi
     fi
 }
