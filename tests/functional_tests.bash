74e71ff990e77f660d1794fffe4c8546c5f89783
Alessandro Sciarra <asciarra@fias.uni-frankfurt.de>
2023-07-17 13:33:28 +0200

    Run functional tests as done for units ones

--- tests/functional_tests.bash
+++ tests/functional_tests.bash
@@ -14,16 +14,21 @@ function Define_Available_Tests()
 
 function Make_Test_Preliminary_Operations()
 {
-    : # No-op for the moment
+    {
+        # Write header to the log file to give some structure to it
+        printf "\n[$(date)]\nRunning test \"%s\"\n\n" "${test_name}"
+        Call_Function_If_Existing_Or_No_Op ${FUNCNAME}__$1
+    } &>> "${HYBRIDT_log_file}" 9>&1 # The fd 9 is used by the logger
 }
 
 function Run_Test()
 {
     local test_name=$1
-    return 0 # Success by definition for the moment
+    Functional_Test__$1 &>> "${HYBRIDT_log_file}" 9>&1  # The fd 9 is used by the logger
 }
 
 function Clean_Tests_Environment_For_Following_Test()
 {
-    : # No-op for the moment
+    # The fd 9 is used by the logger
+    Call_Function_If_Existing_Or_No_Op ${FUNCNAME}__$1 &>> "${HYBRIDT_log_file}" 9>&1
 }
