eaf59e87b4c3fa662800665081ae1a7b1a350c4e
Niklas Götz <goetz@fias.uni-frankfurt.de>
2023-10-26 10:27:18 +0200

    Cleanup after rebase

--- bash/Hydro_functionality.bash
+++ bash/Hydro_functionality.bash
@@ -17,21 +17,11 @@ function Prepare_Software_Input_File_Hydro()
         exit_code=${HYBRID_fatal_file_not_found} Print_Fatal_And_Exit\
             'Base configuration file ' --emph "${HYBRID_software_base_config_file[Hydro]}" ' was not found.'
     fi
-<<<<<<< HEAD
-    # check if config already exists in the output directory, exit if yes
-    local Hydro_input_file_path="${Hydro_output_directory}/vhlle_config"
-    if [[ ! -f "${Hydro_input_file_path}" ]]; then
-        cp "${HYBRID_software_base_config_file['Hydro']}" "${Hydro_input_file_path}" || exit ${HYBRID_fatal_builtin}
-    else
-        exit_code=${HYBRID_fatal_builtin} Print_Fatal_And_Exit\
-            "File \"${Hydro_input_file_path}\" is already there."
-=======
     cp "${HYBRID_software_base_config_file[Hydro]}"\
        "${HYBRID_software_configuration_file[Hydro]}" || exit ${HYBRID_fatal_builtin}
     if [[ "${HYBRID_software_new_input_keys[Hydro]}" != '' ]]; then
         Remove_Comments_And_Replace_Provided_Keys_In_Provided_Input_File\
-            'YAML' "${HYBRID_software_configuration_file[Hydro]}" "${HYBRID_software_new_input_keys[Hydro]}"
->>>>>>> Finalise functionality and test
+            'TXT' "${HYBRID_software_configuration_file[Hydro]}" "${HYBRID_software_new_input_keys[Hydro]}"
     fi
 }
 
@@ -53,20 +43,12 @@ function Ensure_All_Needed_Input_Exists_Hydro()
 
 function Run_Software_Hydro()
 {
-<<<<<<< HEAD
-    local Hydro_input_file_path="${Hydro_output_directory}/${HYBRID_software_base_config_file['Hydro']}"
-    local IC_output_file_path="${IC_output_directory}/SMASH_IC.dat"
-    local Hydro_output_directory="${HYBRID_output_directory}/Hydro"
-    ./"${HYBRID_hydro_software_executable}" "-params" "${Hydro_input_file_path}" 
-        "-ISinput" "${IC_output_file_path}" 
-        "-outputDir" "${HYBRID_software_output_directory[Hydro]}"
-            ">" "${HYBRID_software_output_directory[Hydro]}/Terminal_Output.txt"
-=======
-    local Hydro_input_file_path="${HYBRID_software_output_directory[Hydro]}/${HYBRID_software_input_file[Hydro]}"
-    local IC_output_file_path="${HYBRID_software_output_directory[IC]}/SMASH_IC.dat"
-    local hydro_terminal_output="${HYBRID_software_output_directory[Hydro]}/Terminal_Output.txt"
-    ./"${HYBRID_software_executable[Hydro]}" "-params" "${Hydro_input_file_path}" "-ISinput" "${IC_output_file_path}" "-outputDir" "${HYBRID_software_output_directory[Hydro]}" >> "${hydro_terminal_output}" 
->>>>>>> Finalise functionality and test
+    local -r\
+        hydro_input_file_path="${HYBRID_software_output_directory[Hydro]}/${HYBRID_software_input_file[Hydro]}"\
+        ic_output_file_path="${HYBRID_software_output_directory[IC]}/SMASH_IC.dat"\
+        hydro_terminal_output="${HYBRID_software_output_directory[Hydro]}/Terminal_Output.txt"
+     ./"${HYBRID_software_executable[Hydro]}" "-params" "${Hydro_input_file_path}"\
+         "-ISinput" "${IC_output_file_path}" "-outputDir" "${HYBRID_software_output_directory[Hydro]}" >> "${hydro_terminal_output}" 
 }
 
 
