779daf790a7aa575a0cf4503bd72986e172ee7f4
Alessandro Sciarra <sciarra@itp.uni-frankfurt.de>
2024-02-12 18:28:17 +0100

    Implement scan-prepare command line options parsing
    The new execution mode has options in common with the already existing
    do execution mode. The common options are parsed together in a common
    parser function, but a validation of options per mode has been added and
    performed before, such that it should not be possible to accept wrong
    options. A mechanism to internally replace short options with long ones
    and only deal with the latter has been introduced, too.

--- bash/command_line_parsers/main_parser.bash
+++ bash/command_line_parsers/main_parser.bash
@@ -12,7 +12,8 @@ function Parse_Execution_Mode()
     if [[ "${#HYBRID_command_line_options_to_parse[@]}" -eq 0 ]]; then
         return 0
     fi
-    #Locally set function arguments to take advantage of shift
+    __static__Replace_Short_Options_With_Long_Ones
+    # Locally set function arguments to take advantage of shift
     set -- "${HYBRID_command_line_options_to_parse[@]}"
     case "$1" in
         help | --help)
@@ -26,6 +27,9 @@ function Parse_Execution_Mode()
             ;;
         do)
             HYBRID_execution_mode='do'
+            ;;
+        prepare-scan)
+            HYBRID_execution_mode='prepare-scan'
             ;;
         *)
             exit_code=${HYBRID_fatal_command_line} Print_Fatal_And_Exit \
@@ -45,15 +49,20 @@ function Parse_Execution_Mode()
     fi
 }
 
+# NOTE: The strategy of the following function is to
+#         1) validate if command-line options are allowed in given mode;
+#         2) parse mode-specific options invoking sub-parser;
+#         3) parse remaining options (which are in common to two or more modes) all together.
+#       Since options are validated, step number 3 simplifies the implementation
+#       without the risk of accepting invalid options.
 function Parse_Command_Line_Options()
 {
-    if [[ ${HYBRID_execution_mode} != 'do' ]]; then
-        Print_Internal_And_Exit 'Command line options are allowed only in ' --emph 'do' ' mode for now.'
-    fi
+    __static__Validate_Command_Line_Options
+    Call_Function_If_Existing_Or_No_Op Parse_Specific_Mode_Options_${HYBRID_execution_mode}
     set -- "${HYBRID_command_line_options_to_parse[@]}"
     while [[ $# -gt 0 ]]; do
         case "$1" in
-            -o | --output-directory)
+            --output-directory)
                 if [[ ${2-} =~ ^(-|$) ]]; then
                     Print_Option_Specification_Error_And_Exit "$1"
                 else
@@ -65,7 +74,7 @@ function Parse_Command_Line_Options()
                 fi
                 shift 2
                 ;;
-            -c | --configuration-file)
+            --configuration-file)
                 if [[ ${2-} =~ ^(-|$) ]]; then
                     Print_Option_Specification_Error_And_Exit "$1"
                 else
@@ -81,5 +90,42 @@ function Parse_Command_Line_Options()
         esac
     done
 }
+
+function __static__Replace_Short_Options_With_Long_Ones()
+{
+    declare -A options_map=(
+        ['-c']='--configuration-file'
+        ['-h']='--help'
+        ['-o']='--output-directory'
+        ['-s']='--scan-name'
+    )
+    set -- "${HYBRID_command_line_options_to_parse[@]}"
+    HYBRID_command_line_options_to_parse=()
+    local option
+    for option in "$@"; do
+        if Element_In_Array_Equals_To "${option}" "${!options_map[@]}"; then
+            option=${options_map["${option}"]}
+        fi
+        HYBRID_command_line_options_to_parse+=( "${option}" )
+    done
+}
+
+function __static__Validate_Command_Line_Options()
+{
+    _HYBRID_Declare_Allowed_Command_Line_Options
+    # Let word splitting split valid options
+    local -r valid_options=( ${HYBRID_allowed_command_line_options["${HYBRID_execution_mode}"]} )
+    local option
+    for option in "${HYBRID_command_line_options_to_parse[@]}"; do
+        if [[ ${option} =~ ^- ]]; then
+            if ! Element_In_Array_Equals_To "${option}" "${valid_options[@]}"; then
+                exit_code=${HYBRID_fatal_command_line} Print_Fatal_And_Exit \
+                    'Option ' --emph "${option}" ' is not allowed in ' --emph "${HYBRID_execution_mode}" \
+                    ' execution mode!' 'Use the ' --emph '--help' ' option to get further information.'
+            fi
+        fi
+    done
+}
+
 
 Make_Functions_Defined_In_This_File_Readonly
--- bash/command_line_parsers/sub_parser.bash
+++ bash/command_line_parsers/sub_parser.bash
@@ -1,8 +1,31 @@
 #===================================================
 #
-#    Copyright (c) 2023
+#    Copyright (c) 2024
 #      SMASH Hybrid Team
 #
 #    GNU General Public License (GPLv3 or later)
 #
 #===================================================
+
+function Parse_Specific_Mode_Options_prepare-scan()
+{
+    set -- "${HYBRID_command_line_options_to_parse[@]}"
+    HYBRID_command_line_options_to_parse=()
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+            --scan-name )
+                if [[ ${2-} =~ ^(-|$) ]]; then
+                    Print_Option_Specification_Error_And_Exit "$1"
+                else
+                    readonly HYBRID_scan_directory="${HYBRID_output_directory}/$2"
+                fi
+                shift 2
+                ;;
+            * )
+                HYBRID_command_line_options_to_parse+=( "$1" )
+                shift
+                ;;
+        esac
+    done
+
+}
